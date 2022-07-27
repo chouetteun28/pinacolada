8322702df98c9c9b2b301eca29dd4729edc58285
Gyungmin Myung <ryan000502@gmail.com>
2022-07-28 01:32:21 +0900

    add original audio download

--- utils/downloader.py
+++ utils/downloader.py
@@ -1,7 +1,9 @@
 import pytube
 import os
 import json
-from matplotlib import pyplot as plt
+import sys
+
+DOWNLOAD_RESOLUTION = "480p"
      
 class AssetDatabase:
     def __init__(self):
@@ -21,11 +23,17 @@ class AssetDatabase:
                 self.assets = json.load(f)
         self.length = len(self.assets)
 
-    def add(self, url, song, artist):
+    def add(self, url, song, artist, audio=False):
+        if len(url.split("=")) == 1:
+            print("--------------------------------\nError: Invalid URL\n")
+            print("URL: " + url + "\n")
+
+            return -1
         id = url.split("=")[1]
         if id in self.assets:
             print("--------------------------------\nError: Video already exists in database\n")
             self.logAssets(id)
+            return -1
         else:
             try:
                 yt = pytube.YouTube(url)
@@ -33,31 +41,55 @@ class AssetDatabase:
                 print("--------------------------------\nError: Invalid URL\n")
                 print("URL: " + url + "\n")
 
-                return
+                return -1
             
-            stream = yt.streams.filter(file_extension='mp4').order_by('resolution').desc().first()
-            print(stream)
-            audiostream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
+            if not audio:
+                stream = yt.streams.filter(file_extension='mp4',resolution=DOWNLOAD_RESOLUTION, type="video").desc().first()
+                audiostream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
+                print(stream)
+                print(audiostream)
+                if stream is None:
+                    print("--------------------------------\nError: Video is not available in the selected resolution\n")
+                    return -1
+                if audiostream is None:
+                    print("--------------------------------\nError: Video has no audio\n")
+                    return -1
+                if stream.download(output_path = "./assets/video/", filename = url.split("=")[1]+".mp4") and audiostream.download(output_path = "./assets/audio/", filename = url.split("=")[1]+".mp4"):
+                    self.addToDB( url, artist, song, stream, yt)
+                    return 0
+                else:
+                    print("\nError: Video download failed, \n" + "URL: " + url + "\n")
+                    return -1
+            else: 
+                stream = None
+                audiostream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
+                if audiostream is None:
+                    print("\nError: No audio found\n")
+                    return -1
+                if audiostream.download(output_path = "./assets/audio/", filename = url.split("=")[1]+".mp4"):
+                    self.addToDB( url, artist, song, stream, yt, audio=True)
+                    return 0
+                else:
+                    print("\nError: Video download failed, \n" + "URL: " + url + "\n")
+                    return -1
 
-            if stream is None:
-                print("\nError: No video found\n")
-                return
-            
-            if stream.download(output_path = "./assets/video/", filename = url.split("=")[1]+".mp4") and audiostream.download(output_path = "./assets/audio/", filename = url.split("=")[1]+".mp4"):
-                self.addToDB( url, artist, song, stream, yt)
-            else:
-                print("\nError: Video download failed, \n" + "URL: " + url + "\n")
-                return
-
-    def addToDB(self, url, artist, song, stream, yt):
+    def addToDB(self, url, artist, song, stream, yt, audio=False):
         id = url.split("=")[1]
         self.assets[id] = {
+            "url": url,
             "artist": artist,
             "song": song,
             "length": yt.length,
-            "resolution": stream.resolution,
-            "fps": stream.fps
         }
+        if audio:
+            self.assets[id]["resolution"] = ""
+            self.assets[id]["fps"] = ""
+            self.assets[id]["type"] = "audio"
+        else:
+            self.assets[id]["resolution"] = stream.resolution
+            self.assets[id]["fps"] = stream.fps
+            self.assets[id]["type"] = "video"
+            
         self.length += 1
         with open("./assets/assets.json", "w") as f:
             json.dump(self.assets, f)
@@ -65,11 +97,12 @@ class AssetDatabase:
         self.logAssets(id)
     
     def logAssets(self, id):
-        print("URL: " + "https://www.youtube.com/watch?v=" + id + "\n" + 
+        print("--------------------------------\nURL: " + "https://www.youtube.com/watch?v=" + id + "\n" + 
                 "Artist: " + self.assets[id]["artist"] + "\n" + 
                 "Song: " + self.assets[id]["song"] + "\n" + 
                 "Duration: " + str(self.assets[id]["length"]) + "\n" + 
                 "Resolution: " + self.assets[id]["resolution"] + "\n" + 
+                "Type: " + self.assets[id]["type"] + "\n" +
                 "FPS: " + str(self.assets[id]["fps"]))
 
     def getVideoPath(self, id):
@@ -78,9 +111,70 @@ class AssetDatabase:
     def getAudioPath(self, id):
         return "./assets/audio/" + id + ".mp4"
 
+def main(asset):
+    url = input("Enter URL: ")
+    while 1:
+        if url in asset.assets:
+            print("--------------------------------\nError: Video already exists in database\n")
+            asset.logAssets(url)
+        else:
+            break
+    artist = input("Enter artist: ")
+    while 1:
+        if artist == "":
+            print("--------------------------------\nError: Artist not specified\n")
+        else:
+            counter = 0
+            for key in asset.assets.keys():
+                if asset.assets[key]["artist"] == artist:
+                    asset.logAssets(key)
+                    counter += 1
+            if counter == 0:
+                print("--------------------------------\nNew artist added in database\n")
+            else:
+                print("--------------------------------\n" + str(counter) +" song(s) of this artist found in database\n")
+        break
+    song = input("Enter song: ")
+    while 1:
+        if song == "":
+            print("--------------------------------\nError: Song not specified\n")
+        else:
+            counter = 0
+            for key in asset.assets.keys():
+                if asset.assets[key]["song"] == song:
+                    asset.logAssets(key)
+                    counter += 1
+            if counter == 0:
+                print("--------------------------------\nNew song added in database\n")
+            else:
+                print("--------------------------------\n" + str(counter) +" Song(s) found in database\n")
+        break
+    audioExists = False
+    while 1:
+        for key in asset.assets.keys():
+            if asset.assets[key]["artist"] == artist and asset.assets[key]["song"] == song and asset.assets[key]["type"] == "audio":
+                audioExists = True
+                break
+        if not audioExists:
+            while 1: 
+                print("--------------------------------\nNo audio found for this song")
+                audio_url = input("Enter URL of original audio: ")
+                if audio_url in asset.assets:
+                    print("--------------------------------\nError: Audio already exists in database\n")
+                    asset.logAssets(audio_url)
+                else:
+                    break
+            if asset.add(audio_url, song, artist, audio=True) == 0:
+                break
+        else:
+            break
+    if asset.add(url, song, artist) == 0:
+        return 0
+
+
 # execute only from this file
 if __name__ == "__main__":
     asset = AssetDatabase()
-
-    asset.add("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "The Sign", "Ace of Base")
-    asset.add("https://www.youtube.com/watch?v=dQw4Q", "The Sign", "Ace of Base")
+    while 1:
+        if main(asset) == 0:
+            break
--- utils/videoloader.py
+++ utils/videoloader.py
@@ -15,10 +15,7 @@ while True:
     if frame is None:
         break
 
-    # invert image color
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = cv2.flip(frame, 1)
-    cv2.imshow("Video", frame)
+    cv2.imshow("frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
